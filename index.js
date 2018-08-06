6ce67b575c7566b460577fd67388118626248414
Mircea Ciurea <mciurea@pentalog.com>
2018-08-06 11:03:37 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const app = require('express')();
+const http = require('http').Server(app);
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, ()=>{
+    console.log('server started on port 3000');
+});
