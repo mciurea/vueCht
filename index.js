6b3209c08198e2d39f5a90edcc737bd700d20412
Mircea Ciurea <mciurea@pentalog.com>
2018-08-06 12:15:52 +0300

    added socket io

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const app = require('express')();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/index.html');
 });
 
+
+io.on('connection', (socket)=>{
+    console.log("some user connected");
+})
+
+
 http.listen(3000, ()=>{
     console.log('server started on port 3000');
 });
-
